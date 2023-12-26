3cdf0a9d827f5cd30e316e7fd8f4fc3204edf1ae
anurag singh <anurag6569201@gmail.com>
2023-12-27 00:35:55 +0530

    adjusted the css and make it responsive

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -4,11 +4,10 @@ window.addEventListener("scroll", () => {
   console.log(offset);
   if (offset >= 10) {
     navbar.style.background= "rgba(255, 255, 255, 1)";
-    navbar.style.boxShadow = "0 0 3px 10px white";
+    navbar.style.boxShadow = "0 0 1px 3px white";
   }
   else {
     navbar.style.background = "rgba(255, 255, 255, 0)";
-    navbar.style.boxShadow = "0 0 3px 10px transparent";
+    navbar.style.boxShadow = "0 0 1px 3px transparent";
   }
-
 });
