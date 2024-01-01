9466212ae785605b0374aba587e4af19f3236fac
anurag singh <anurag6569201@gmail.com>
2024-01-01 14:33:16 +0530

    added vercel settings

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nkm=6aoslh$o_&*eg2f^rtw@5l^k%14dn)m0l5czfi+tdg^e1a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '[*]'
 
 
 # Application definition
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'ecomprj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
