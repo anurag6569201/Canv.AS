ec8f75e85835b103a90f2553c2c2c9375ea7d6f3
anurag singh <anurag6569201@gmail.com>
2024-01-12 09:44:29 +0530

    fixed all bugs ,creating deleting product cats user everything added good models

--- userauths/models.py
+++ userauths/models.py
@@ -1,6 +1,7 @@
 from enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
@@ -9,6 +10,23 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    # Add related_name to avoid clashes with auth.User
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="userauths_user_set",
+        blank=True,
+        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+        verbose_name="groups",
+    )
+    
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="userauths_user_set",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
 
     def __str__(self):
         return self.username
